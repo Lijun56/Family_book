418394c7692dee45ff475d997f8e8a1c598d5b29
Lijun Zhu <zhu944@purdue.edu>
2022-06-11 15:29:25 -0400

    demo2: ARview and photoPickerView

---
+++ Famility_Book/File.swift
@@ -0,0 +1,23 @@
+import Foundation
+import UIKit
+
+struct segue{
+    static let WelcomeToAR = "ARup"
+}
+
+
+
+
+
+//struct AVData{
+//    var delegate: AVDelegate?
+//}
+//    var Image: UIImage
+//    var VideoUrl: URL
+//
+//    mutating func Transfer(){
+//        if let result = delegate?.GetData(){
+//            self.Image = result.0
+//            self.VideoUrl = result.1
+//        }
+//    }
